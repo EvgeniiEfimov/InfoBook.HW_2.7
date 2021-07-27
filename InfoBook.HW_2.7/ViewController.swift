b2a24fbd05f8af0fec69f51062ee50beeb152541
1.0.0 <1.0.0>
2021-07-27 14:22:16 +0300

    Initial Commit

---
+++ InfoBook.HW_2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfoBook.HW_2.7
+//
+//  Created by User on 27.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
